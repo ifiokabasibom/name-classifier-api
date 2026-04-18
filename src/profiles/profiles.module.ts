881076b98f53ab587faa14c5cac16a8d3d7f0df5
Wisdom-E-A <wisdom@esca.finance>
2026-04-18 13:28:18 +0100

    fix: register ExternalService in ProfilesModule

--- src/profiles/profiles.module.ts
+++ src/profiles/profiles.module.ts
@@ -3,13 +3,20 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProfilesController } from './profiles.controller';
 import { ProfilesService } from './profiles.service';   
 import { ProfileEntity } from './profile.entity/profile.entity';
+import { ExternalModule } from '../external/external.module';
+
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ProfileEntity])],
+  imports: [
+    TypeOrmModule.forFeature([ProfileEntity]),
+    ExternalModule,
+  ],
   controllers: [ProfilesController],
-  providers: [ProfilesService]
+  providers: [ProfilesService],
 })
 export class ProfilesModule {}
+
+
 
 
 
